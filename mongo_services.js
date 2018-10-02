df94d25cb7a064d4a2f951232ea2b18144c7bcca
Luis Emilio Juarez <luis_juarez2394@hotmail.com>
2018-10-01 23:47:20 -0600

    cambios para hacer metodos tipo post

--- mongo_services.js
+++ mongo_services.js
@@ -1,27 +1,45 @@
 //declaracion de cliente mongo db y ConnectionString
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/surveys";
+var url = "mongodb://localhost:27017/";
 var express = require("express");
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 //declaracion de los servicios API
-app.get("/url", (req, res, next) => {
+app.get("/survey/answer", (req, res, next) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
    });
 
+app.post('/survey/answer', function(req, res) {
+    var survey_answer = req.body.survey_answer;
+    insertSurvey(survey_answer)
+    res.send(JSON.stringify(survey_answer));
+});
+
 //levantamos el servidor
-function server_creation(){
+function createServer(){
     app.listen(3000, () => {
     console.log("Server running on port 3000");
-    creating_apis();
+    // createConn();
     });
 }
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  // llamar el servidor que cree las apis que vamos a consumir.
-  server_creation();
-  db.close();
-  console.log("DbConnection closed");
-});
+//levantamos la conexion a la base de datos
+function insertSurvey(myobj)
+{
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        console.log("connected to Database");
+        var dbo = db.db("surveys");
+        dbo.collection("answers").insertOne(myobj, function(err, res) {
+          if (err) throw err;
+          console.log("1 document inserted");
+          db.close();
+        });
+      }); 
+}
+
+//ejecuccion 
+createServer();
